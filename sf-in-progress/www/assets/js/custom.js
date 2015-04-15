b6064c8fb10dce9f1d2eb98872a1db4e89a0ae2b
nora humpage <nora.humpage@gmail.com>
2015-04-15 16:26:35 -0700

    finished slide through slide-5

--- sf-in-progress/www/assets/js/custom.js
+++ sf-in-progress/www/assets/js/custom.js
@@ -5,10 +5,7 @@ $(document).ready(function(){
 
         // Init Skrollr
         var s = skrollr.init({
-            render: function(data) {
-                //Debugging - Log the current scroll position.
-                console.log(data.curTop);
-            }
+            forceHeight:false
         });
 
         s.refresh($('.homeSlide'));
